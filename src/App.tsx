7a33967574c84e730c572e5bea8dcd9ce3c9b069
kolen44 <malavov70@gmail.com>
2024-02-27 20:32:24 +0300

    feat: Added face tracking Using faceLandmarker, we were able to connect to the mediaPipe and start tracking the face by entering the main points into the console

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,32 @@
+import { FaceLandmarker, FilesetResolver } from '@mediapipe/tasks-vision'
 import { useGLTF } from '@react-three/drei'
 import { Canvas, useGraph } from '@react-three/fiber'
 import { useEffect } from 'react'
 import { Color } from 'three'
 import './App.css'
 
-let video: HTMLVideoElement
+let video: HTMLVideoElement,
+	// eslint-disable-next-line @typescript-eslint/no-unused-vars
+	faceLandmarker: FaceLandmarker,
+	lastVideoTime = -1
 
 function App() {
 	function handleOnChange() {}
 
-	const setup = () => {
+	const setup = async () => {
+		const vision = await FilesetResolver.forVisionTasks(
+			// path/to/wasm/root
+			'https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@latest/wasm'
+		)
+		faceLandmarker = await FaceLandmarker.createFromOptions(vision, {
+			baseOptions: {
+				modelAssetPath: `https://storage.googleapis.com/mediapipe-models/face_landmarker/face_landmarker/float16/1/face_landmarker.task`,
+				delegate: 'GPU',
+			},
+			outputFaceBlendshapes: true,
+			outputFacialTransformationMatrixes: true,
+			runningMode: 'VIDEO',
+		})
 		video = document.getElementById('video') as HTMLVideoElement
 		navigator.mediaDevices
 			.getUserMedia({
@@ -17,7 +34,18 @@ function App() {
 			})
 			.then(stream => {
 				video.srcObject = stream
+				video.addEventListener('loadeddata', predict)
 			})
+	}
+
+	const predict = () => {
+		const nowInMs = Date.now()
+		if (lastVideoTime !== video.currentTime) {
+			lastVideoTime = video.currentTime
+			const result = faceLandmarker.detectForVideo(video, nowInMs)
+			console.log(result)
+		}
+		requestAnimationFrame(predict)
 	}
 
 	useEffect(() => {
@@ -59,7 +87,7 @@ function App() {
 
 function Avatar() {
 	const avatar = useGLTF(
-		'https://models.readyplayer.me/65ddfac21699818a1b71f6e3.glb?morphTargets=ARKit'
+		'https://models.readyplayer.me/65ddfac21699818a1b71f6e3.glb?morphTargets=ARKit&textureAtlas=1024'
 	)
 	const { nodes } = useGraph(avatar.scene)
 	return <primitive object={avatar.scene} position={[0, -1.65, 4]} />
