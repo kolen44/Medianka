69c3f2ec25aee402d66318d469ea779d2f424df9
kolen44 <malavov70@gmail.com>
2024-03-07 23:49:54 +0300

    feat: Added 3D model with movement Was added glb model into public folder and import into canvas .

---
+++ src/components/AvatarCanvas.tsx
@@ -0,0 +1,49 @@
+import { Html, OrbitControls, useAnimations, useGLTF } from '@react-three/drei'
+import { Canvas } from '@react-three/fiber'
+import { useEffect, useState } from 'react'
+
+const Avatar = () => {
+	const [url, setUrl] = useState('/Defeated.glb')
+	const avatar = useGLTF(url)
+	const [index, setIndex] = useState(1)
+	const { actions, names } = useAnimations(avatar.animations, avatar.scene)
+	const [isClicked, setClicked] = useState(false)
+	useEffect(() => {
+		actions[names[index]]?.reset().fadeIn(0.5).play()
+		return () => {
+			actions[names[index]]?.fadeOut(0.5)
+		}
+	}, [index, actions, names])
+	return (
+		<group>
+			<primitive
+				object={avatar.scene}
+				scale={2}
+				position={[-1, -2, 0]}
+				rotation-y={0.5}
+			/>
+			<Html position={[-3.7, 0.3, 0]}>
+				<button
+					onClick={() => {
+						setIndex((index + 1) % names.length)
+						setClicked(!isClicked)
+					}}
+				>
+					Click me
+				</button>
+			</Html>
+		</group>
+	)
+}
+
+export default function AvatarCanvas() {
+	return (
+		<Canvas>
+			<pointLight position={[0, 1.5, 0]} intensity={1.5} />
+			<ambientLight intensity={0.5} position={[1, 1.8, 2]} />
+			<OrbitControls position={[1, 1, 1]} />
+
+			<Avatar />
+		</Canvas>
+	)
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import './components/AvatarCanvas.css'
+import AvatarCanvas from './components/AvatarCanvas.tsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+	<React.StrictMode>
+		<div className='canvas3d'>
+			<AvatarCanvas />
+		</div>
+	</React.StrictMode>
 )
