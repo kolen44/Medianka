3b75e19a2088f5636ef8a11dc931a9bf9b7dd42e
kolen44 <malavov70@gmail.com>
2024-03-08 18:03:39 +0300

    feat: Added frontend of speech recognition Added SpeechToText.tsx into components directory and updated file of main styles - App.css wich coresponding of SpeechToText.tsx

---
+++ src/components/SpeechToText.tsx
@@ -0,0 +1,16 @@
+export default function SpeechToText() {
+	return (
+		<div className='container'>
+			<h2>Speech to text</h2>
+			<p>Converter</p>
+
+			<div className='main-content'></div>
+
+			<div className='btn-style'>
+				<button>copy</button>
+				<button>start</button>
+				<button>stop</button>
+			</div>
+		</div>
+	)
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import './App.css'
 import './components/AvatarCanvas.css'
 import AvatarCanvas from './components/AvatarCanvas.tsx'
+import SpeechToText from './components/SpeechToText.tsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
 		<div className='canvas3d'>
 			<AvatarCanvas />
+			<SpeechToText />
 		</div>
 	</React.StrictMode>
 )
