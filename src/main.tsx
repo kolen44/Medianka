906aaa9eea13c09e464365c9f30dc2a4bfb8cf07
kolen44 <malavov70@gmail.com>
2024-03-10 18:15:37 +0300

    feat: Add 3D scene setup with GLTF model rendering Add 3D scene setup with GLTF model rendering, lighting, camera controls, and effects Added code to create a 3D scene using Three.js libraries. The model is rendered from the hall-transformed.glb file using the <Gltf /> component, lighting and effects are added, as well as a camera with the ability to control.

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './App.css'
-import './components/AvatarCanvas.css'
-import AvatarCanvas from './components/AvatarCanvas.tsx'
-import SpeechToText from './components/SpeechToText.tsx'
+import Experience from './components/Experience.tsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
 		<div className='canvas3d'>
-			<AvatarCanvas />
-			<SpeechToText />
+			{/* <AvatarCanvas />
+			<SpeechToText /> */}
+			<Experience />
 		</div>
 	</React.StrictMode>
 )
