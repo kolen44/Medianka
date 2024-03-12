248adbf5300e0db407937202520d459bf97ac4e6
Aspir01 <nikitovicaleks488@gmail.com>
2024-03-12 19:52:15 +0300

    добавил карту

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,33 @@
+import { useState } from 'react'
 import './App.css'
 import Experience from './components/Experience'
 import SpeechToText from './components/SpeechToText'
+import { YMaps, Placemark, Map } from '@pbe/react-yandex-maps'
 
 function App() {
+
+	const [opened, setOpened] = useState(false)
+
+	function handleClick() {
+		setOpened(!opened)
+	}
+
 	return (
 		<>
 			<div className='canvas3d'>
 				<Experience />
 				<SpeechToText />
 			</div>
+			<button onClick={handleClick} className='btn map'>
+				Открыть карту
+			</button>
+			{opened &&
+				<div className='maps'>
+					<YMaps>
+						<Map width={'300px'} defaultState={{ center: [56.85, 60.61], zoom: 10 }} modules={["control.ZoomControl", "control.FullscreenControl"]} />
+						<Placemark defaultGeometry={[56.85, 60.61]} />
+					</YMaps>
+				</div>}
 		</>
 	)
 }
