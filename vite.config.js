e79cf255fad63ce2171f5e13e8d90f9737346298
kolen44 <malavov70@gmail.com>
2024-03-13 01:59:04 +0300

    fix(api)!: CORS Error Cors Error for vite was fixed by adding proxy parametr into vite.config.js file .

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+	plugins: [react()],
+	server: {
+		proxy: {
+			'/api': {
+				target: 'https://api.openai.com/v1/chat/completions',
+				changeOrigin: true,
+			},
+		},
+	},
 })
